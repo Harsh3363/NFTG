829981681add90ffcc276b9be750b8718880e15a
Harsh Kanodiya <harsh.kanodiya@gmail.com>
2022-10-18 00:37:04 +0530

    first commit

---
+++ ecom/nextjs-starter-medusa-main/src/modules/home/components/hero/index.tsx
@@ -0,0 +1,32 @@
+import UnderlineLink from "@modules/common/components/underline-link"
+import Image from "next/image"
+
+const Hero = () => {
+  return (
+    <div className="h-[90vh] w-full relative">
+      <div className="text-white absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:text-left small:justify-end small:items-start small:p-32">
+        <h1 className="text-2xl-semi mb-4 drop-shadow-md shadow-black">
+          Valuable NFT's are here
+        </h1>
+        <p className="text-base-regular max-w-[32rem] mb-6 drop-shadow-md shadow-black">
+          This year, our new NFT collection will add value to your Profile in this world that 
+          doesn&apos;t care if you live or die.
+        </p>
+        <UnderlineLink href="/store">Explore NFT's</UnderlineLink>
+      </div>
+      <Image
+        src="/hero.jpg"
+        layout="fill"
+        loading="eager"
+        priority={true}
+        quality={90}
+        objectFit="cover"
+        alt="Photo by @thevoncomplex https://unsplash.com/@thevoncomplex"
+        className="absolute inset-0"
+        draggable="false"
+      />
+    </div>
+  )
+}
+
+export default Hero
